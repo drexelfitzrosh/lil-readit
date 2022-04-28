936a457d9e052233974798903ebdd997f46ae32d
drexelfitzrosh <fitzrosh@gmail.com>
2022-04-28 13:30:49 +0800

    Add Auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  login() {
+    return { msg: 'login' };
+  }
+  signup() {
+    return { msg: 'signup' };
+  }
+}
